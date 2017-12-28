65a02f0615108c018aca1529f0d560c508170eb5
Fu Ck U <d.foxultra@gmail.com>
2017-12-27 19:07:36 -0600

    application schema added

---
+++ models/Application.js
@@ -0,0 +1,68 @@
+var moment = require("moment");
+
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var ApplicationSchema = new Schema({
+  //first Name
+  firstname: {
+    type: String,
+    required: true
+    },
+//middle initial
+  middleinitial: {
+    type: String,
+    required: false
+  },
+// Last Name
+  lastname: {
+    type: String,
+    required: true
+  },
+  //Cumulative Address
+  address: {
+    type: String,
+    required: true
+  },
+  // Applicant's City
+  city: {
+    type: String,
+    required: true
+  },
+
+  // Applicant's State
+  state: {
+    type: String,
+    required: true
+  },
+
+  // Applicant's Zip Code
+  zipcode: {
+    type: String,
+    required: true
+  },
+  //Applicant's Phone Number
+  phonenumber:{
+    type: String,
+    required: true
+  },
+  email: {
+      type: String,
+      required: true
+  }
+  socialsecurity:{
+    type: String,
+    required: true
+  }
+  update:{
+    type: String,
+    default: moment().format('MMMM Do YYYY, h:mm A')
+  },
+  customerid: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Accounts'
+  }]
+
+
+})
